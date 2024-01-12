49be16f297f8aecee033077400a10f42c31200be
Anand Singh <pb11aps97@gmail.com>
2024-01-12 18:18:49 -0500

    login page import fixed.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Header } from "./components/header/header.component";
 import {Route, Routes } from "react-router-dom";
 import { IndexPage } from "./pages/indexPage";
-import { LoginPage } from "./pages/loginPage";
+import LoginPage from "./pages/loginPage";
 import { RegisterPage } from "./pages/registerPage";
 import { CreatePost } from "./pages/createPostPage";
 import { PostDetails } from "./pages/postDetailsPage";
